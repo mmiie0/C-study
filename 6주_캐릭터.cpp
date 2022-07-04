73b7050cacfe648fecef50bd6fb6f43c5dca63f7
mmiie0 <102473425+mmiie0@users.noreply.github.com>
2022-07-05 08:11:03 +0900

    Update 6주_캐릭터.cpp

--- 6주_캐릭터.cpp
+++ 6주_캐릭터.cpp
@@ -10,23 +10,19 @@ private:
 	char* job;
 
 public:
-	Character(const char* name, int age, const char* job); //생성자
+	Character(const char* name, int age, char* job); //생성자
 	Character(const Character& character); //복사 생성자
-	~Character(); //소멸자
+	
 	void show() {
-		cout << "******캐릭터 정보******" << endl;
+		cout << "\n******캐릭터 정보******\n";
 		cout << "캐릭터 이름: " << name << endl;
 		cout << "캐릭터 나이: " << age << endl;
 		cout << "캐릭터 직업: " << job << "\n\n";
 	}
-	void charac_change(const char* name, int age, const char* job) {
-		strcpy(this->name, name); //name에 문자열 복사
-		this->age = age; //age값 복사
-		strcpy(this->job, job); //job에 문자열 복사
-	}
+	void charac_change(const char* name, int age, const char* job) {};
 };
 
-Character::Character(const char* name, int age, const char* job) { //생성자
+Character::Character(const char* name, int age, char* job) { //생성자
 	int len_name = strlen(name); //name의 문자 개수
 	this->name = new char[len_name + 1]; //name 문자열 공간 할당
 	strcpy(this->name, name); //name에 문자열 복사
@@ -50,28 +46,16 @@ Character::Character(const Character& character) { //복사 생성자
 	strcpy(this->job, character.job); //job의 문자열 복사
 }
 
-/////////////////////이 부분 필요한 건가요??
-Character::~Character() { //소멸자
-	while (1) {
-		if (name) //만일 name에 동적 할당된 배열이 있으면
-			delete[] name; //동적 할당 메모리 소멸
-		else if (job) //만일 job에 동적 할당된 배열이 있으면
-			delete[] job; //동적 할당 메모리 소멸
-	}
-}
-
 
 int main() {
-	char charac_name[20], charac_name2[20];
-	int charac_age, charac_age2;
-	char charac_job[20], charac_job2[20];
-
-	Character A(charac_name, charac_age, charac_job);
-	Character B(charac_name2, charac_age2, charac_job2);
-
 	while (1) {
+		char name[20];
+		int age = 0;
+		char job[20];
 		int num;
-		Character copy(A);
+
+		Character A(name, age, job);
+		Character B(A);
 
 		cout << endl << "========캐릭터 관리========\n\n";
 		cout << "1. 캐릭터 생성" << endl;
@@ -85,44 +69,81 @@ int main() {
 		if (num == 1) { //캐릭터 생성
 			cout << "\n생성할 캐릭터의 이름, 나이, 직업을 순서대로 입력해주세요.(공백으로 구분)" << endl;
 			cout << "참고사항: 직업의 종류는 자유롭게\n--> ";
-			cin >> charac_name >> charac_age >> charac_job; //그냥 받았더니 됐다??
+			cin >> name >> age >> job;
+			Character A(name, age, job);
 			cout << "\n캐릭터가 생성되었습니다." << endl;
-		}
 
-		if (num == 2) { //캐릭터 복사
-			Character copy(A);
-			cout << "\n캐릭터가 복사되었습니다." << endl;
-		}
+			while (1) {
+				cout << endl << "========캐릭터 관리========\n\n";
+				cout << "1. 캐릭터 생성" << endl;
+				cout << "2. 캐릭터 복사" << endl;
+				cout << "3. 캐릭터 정보 확인" << endl;
+				cout << "4. 캐릭터 정보 수정" << endl;
+				cout << "5. 종료" << endl;
+				cout << "\n번호를 선택해주세요: ";
+				cin >> num;
 
-		if (num == 3) { //캐릭터 정보 확인
-			A.show(); //사람1 출력
-			if (charac_name)
-				B.show(); //사람2 출력
-		}
+				if (num == 2) {
+					Character B(A);
+					cout << "\n캐릭터가 복사되었습니다." << endl;
 
-		if (num == 4) { //캐릭터 정보 수정
-			int charac_num;
+					while (1) {
+						cout << endl << "========캐릭터 관리========\n\n";
+						cout << "1. 캐릭터 생성" << endl;
+						cout << "2. 캐릭터 복사" << endl;
+						cout << "3. 캐릭터 정보 확인" << endl;
+						cout << "4. 캐릭터 정보 수정" << endl;
+						cout << "5. 종료" << endl;
+						cout << "\n번호를 선택해주세요: ";
+						cin >> num;
 
-			cout << "수정하고 싶은 캐릭터를 선택하세요.(1 or 2): ";
-			cin >> charac_num;
-			cout << "설정하고 싶은 이름, 나이, 직업을 입력해주세요.\n--> ";
+						if (num == 3) { //
+							A.show();
+							B.show();
+						}
 
-			if (charac_num == 1) {
-				cin >> charac_name2 >> charac_age2 >> charac_job2; //새로 입력 받는다
-				A.charac_change(charac_name2, charac_age2, charac_job2);
+						if (num == 4) {
+							int charac_num;
+
+							cout << "수정하고 싶은 캐릭터를 선택하세요.(1 or 2): ";
+							cin >> charac_num;
+							cout << "설정하고 싶은 이름, 나이, 직업을 입력해주세요.\n--> ";
+
+							if (charac_num == 1) {
+								cin >> name >> age >> job;
+								A.charac_change(name, age, job);
+							}
+
+							else if (charac_num == 2) {
+								cin >> name >> age >> job;
+								B.charac_change(name, age, job);
+							}
+
+							cout << "\n정상적으로 수정되었습니다.\n";
+
+						}
+
+						else if (num == 4) { //캐릭터 정보 수정
+							cout << "설정하고 싶은 이름, 나이, 직업을 입력해주세요.\n--> ";
+							cin >> name >> age >> job;
+							A.charac_change(name, age, job);
+						}
+
+						else if (num == 5) {
+							cout << "종료되었습니다." << endl;
+							break;
+						}
+					}
+				}
+				else if (num == 3)
+					A.show();
 			}
-
-			if (charac_num == 2) {
-				cin >> charac_name2 >> charac_age2 >> charac_job2;
-				B.charac_change(charac_name2, charac_age2, charac_job2);
-			}
-			cout << "\n정상적으로 수정되었습니다.\n";
 		}
 
-		if (num == 5)  { //종료
+		else if (num == 5) { //종료
 			cout << "종료되었습니다." << endl;
 			break;
 		}
+		return 0;
+		}
 	}
-	return 0;
-}
